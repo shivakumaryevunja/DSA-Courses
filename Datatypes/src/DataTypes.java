a59abf1e4765718c21c53fc6fb504a3e1abd1f14
shivakumar <shivakumar.yevunja@gmail.com>
2023-10-01 20:38:55 +0530

    datatype

--- Datatypes/src/DataTypes.java
+++ Datatypes/src/DataTypes.java
@@ -1,2 +1,9 @@
-package PACKAGE_NAME;public class DataTypes {
+public class DataTypes {
+    public static void main(String[] args){
+        int x = 32;
+        float y = 44.87665f;
+        double value = 23.784778;
+        String name = "Joy";
+        System.out.println(x+y+value+);
+    }
 }
