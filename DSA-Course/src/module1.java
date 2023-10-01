d1a6ae4e0bdf858b7d9f95a8314c147b00624c5b
shivakumar <shivakumar.yevunja@gmail.com>
2023-10-01 21:32:58 +0530

    newchanges

--- DSA-Course/src/module1.java
+++ DSA-Course/src/module1.java
@@ -1,6 +1,20 @@
+import javax.xml.transform.stream.StreamSource;
+import java.util.Scanner;
 public class module1 {
     public static void main(String[] args){
-        int a =444444444;
-        System.out.println(a);
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the 4 digit number to check:");
+        int a = sc.nextInt();
+        int th = a/1000;
+        System.out.println(th+" is in the thousands place");
+        int hu = (a%1000)/100;
+        System.out.println(hu+" is in the hundreds place");
+        int tens = (a%100)/10;
+        System.out.println(tens+" is in the hundreds place");
+        int ones = a%10;
+        System.out.println(ones+" is in the hundreds place");
+
+        int[] arr = {3,2,4,5};
     }
 }
+
