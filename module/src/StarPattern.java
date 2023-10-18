c541b72cc0a1d1c992ee42a0a1ad1dfcec9aae13
shivakumar <shivakumar.yevunja@gmail.com>
2023-10-18 23:31:26 +0530

    start pattern code

--- module/src/StarPattern.java
+++ module/src/StarPattern.java
@@ -1,14 +1,14 @@
 public class StarPattern {
         public static void main(String[] args){
-            int num =1;
-            int n = 4;
+            //int num =1;
+            int n = 3;
             for(int i=1;i<=n;i++){
                 for(int j=1;j<=n-i;j++){
                     System.out.print(" ");
                 }
                 for(int j=1;j<=i;j++){
                     System.out.print("*"+" ");
-                    num++;}
+                    }
                 for(int j=n-i;j>=1;j--){
                     System.out.print(" ");
                 }
@@ -21,7 +21,7 @@ public class StarPattern {
                 }
                 for(int j=1;j<=i;j++){
                     System.out.print("*"+" ");
-                    num++;}
+                    }
 
                 for(int j=n-i;j>=1;j--){
                     System.out.print(" ");
